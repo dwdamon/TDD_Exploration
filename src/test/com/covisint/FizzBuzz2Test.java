4e6e4a35e475d2850d1f75e2d9e78b08bc3d99b2
David Damon <david.damon@covisint.com>
2018-02-27 12:57:45 -0500

    third kata of FizzBuzz

---
+++ src/test/com/covisint/FizzBuzz2Test.java
@@ -0,0 +1,39 @@
+package com.covisint;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance( TestInstance.Lifecycle.PER_CLASS )
+public class FizzBuzz2Test {
+  @Test
+  public void shoudlReturnFizz() {
+    assertEquals( "Fizz", FizzBuzz2.evaluate( 3 ));
+    assertEquals( "Fizz", FizzBuzz2.evaluate( 6 ));
+    assertEquals( "Fizz", FizzBuzz2.evaluate( 9 ));
+    assertEquals( "Fizz", FizzBuzz2.evaluate( 24 ));
+  }
+  
+  @Test
+  public void shouldReturnBuzz() {
+    assertEquals( "Buzz", FizzBuzz2.evaluate( 5 ));
+    assertEquals( "Buzz", FizzBuzz2.evaluate( 10 ));
+    assertEquals( "Buzz", FizzBuzz2.evaluate( 20 ));
+    assertEquals( "Buzz", FizzBuzz2.evaluate( 25 ));
+  }
+  
+  @Test
+  public void shouldReturnFizzBuzz() {
+    assertEquals( "FizzBuzz", FizzBuzz2.evaluate( 15 ));
+    assertEquals( "FizzBuzz", FizzBuzz2.evaluate( 30 ));
+  }
+  
+  @Test void shouldReturnNumber() {
+    assertEquals( "2", FizzBuzz2.evaluate( 2 ));
+    assertEquals( "4", FizzBuzz2.evaluate( 4 ));
+    assertEquals( "7", FizzBuzz2.evaluate( 7 ));
+    assertEquals( "23", FizzBuzz2.evaluate( 23 ));
+  }
+  
+}
